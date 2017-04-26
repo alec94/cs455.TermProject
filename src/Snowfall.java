87379fc850d66a3db19cccc8154efa472bdc2f38
JAGCSU <justin.glover@colostate.edu>
2017-04-26 08:15:21 -0600

    NOT-WORKING tuple aggregation functions added, nearly working but line 52 t._1 and t._2 aren't recognized symbols

--- src/Snowfall.java
+++ src/Snowfall.java
@@ -1,6 +1,11 @@
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
+import scala.Tuple3;
 
 /**
  * Created by Justin on 4/23/2017.
@@ -8,6 +13,7 @@ import org.apache.spark.storage.StorageLevel;
 public class Snowfall {
 
 	public static JavaRDD<String> filterSnowfall(JavaRDD<Summary> coData) {
+
 		// get total monthly snowfall
 		JavaRDD<String> coSnowfall = coData.filter(
 				(Function<Summary, Boolean>) line -> line.getElement().equals("SNOW")
@@ -27,9 +33,25 @@ public class Snowfall {
 						}
 					}
 
-					return year + "," + month + "," + snowfall;
+					return year + month + "," + snowfall;
 				}
 		).persist(StorageLevel.MEMORY_ONLY());
+
+		//  aggregate month values
+		JavaPairRDD<String, Tuple2<Integer, Integer>> pairRDD = coSnowfall.mapToPair(s -> {
+			String[] tokens = s.split(",");
+			return new Tuple2(tokens[0], new Tuple2(Integer.parseInt(tokens[1]), 1));
+		});
+
+		JavaPairRDD<String, Tuple2<Integer, Integer>> snowAverages = pairRDD.reduceByKey(
+				(Function2<Tuple2<Integer, Integer>, Tuple2<Integer,Integer>, Tuple2<Integer, Integer>>)
+						(acc, val) -> new Tuple2(acc._1 + val._1, acc._2 + val._2)
+		);
+
+		JavaPairRDD<String, Integer> finalStuff = snowAverages.mapToPair(
+				(PairFunction<String, Tuple2<Integer, Integer>, Tuple2<String, Integer>>) (s, t) -> new Tuple2(s, t._1 / t._2)
+		);
+//				(Function<Tuple2<String, Integer>, Integer>) (sumCount, count) -> sumCount._2 / count); // use map here instead? how do i get the count and divide it by the sum here??
 
 		return coSnowfall;
 	}
